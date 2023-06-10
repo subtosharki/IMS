3bd162e4aaa4fb7c43090ed75c978eef29dfbdeb
subtosharki <camywam@yahoo.com>
2023-06-10 11:52:09 -0400

    forgot duh

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,8 @@
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import {SvelteKitPWA} from "@vite-pwa/sveltekit";
 
 export default defineConfig({
-	plugins: [sentrySvelteKit(), sveltekit()],
+	plugins: [sentrySvelteKit(), sveltekit(), SvelteKitPWA()],
 });
